0b2ed484c9c77cc87047185a386cf5852e401ec3
DirtyDragon-CCXV <175989130+DirtyDragon-CCXV@users.noreply.github.com>
2026-03-14 17:26:12 -0400

    new style to the cv

--- docs/engine.js
+++ docs/engine.js
@@ -14,244 +14,50 @@
  * ==============================================================================
  */
 
-// --- dark mode change colors ---
-const SWITCH_MODE = document.getElementById("darkmode");
+let navbar_status = 0; //0 to closed, 1 to open
+let navbar_trigger = document.getElementsByClassName("navbar_trigger")[0];
+let navbar = document.getElementsByTagName("nav")[0];
 
-const HEADER = document.getElementById("top");
-const STUDIES = document.getElementById("studies");
-const KNOWLEDGE = document.getElementById("knowledge");
-const SVG = document.getElementsByTagName("svg");
-const LANGUAGES_CUAD_BOX = document.getElementsByClassName("cuad_box");
-const FOOTER = document.getElementsByTagName("footer")[0];
-const ICONS = document.getElementsByClassName("icon");
-const LINKEDIN_ICON = document.getElementsByClassName("linkedin_icon")[0];
-const LINKEDIN_QR = document.getElementsByClassName("linkedin_qr")[0];
+let navbar_links = navbar.children;
+let ids = ["main", "studies", "lab_exp", "languages", "mind", "contact"]
+let scroll_elements = []
 
-// darkmode position
-window.addEventListener("load", () => {
-    SWITCH_MODE.style.top = HEADER.offsetHeight + 20 + "px";
-});
+for (let z = 0; z < ids.length; z++) {
+    const element = ids[z];
 
-let flag_mode = 0; // 0 to light, 1 to dark
-
-if (screen.width >= 1000) {
-    var mouserInHover = SWITCH_MODE.addEventListener("mouseenter", () => {
-        SWITCH_MODE.style.backgroundColor = "#323c72";
-        SWITCH_MODE.style.borderColor = "#111";
-    });
-
-    var mouseOutHover = SWITCH_MODE.addEventListener("mouseleave", () => {
-        SWITCH_MODE.style.backgroundColor = "#669";
-        SWITCH_MODE.style.borderColor = "#336";
-    });
+    scroll_elements.push(document.getElementById(element));
 }
 
-SWITCH_MODE.addEventListener("click", () => {
-    if (flag_mode == 0) {
-        SWITCH_MODE.children[1].removeAttribute("hidden");
-        SWITCH_MODE.children[0].setAttribute("hidden", "true");
-        SWITCH_MODE.style.backgroundColor = "#e8e8c7";
-        SWITCH_MODE.style.borderColor = "#c4bf5c";
-        SWITCH_MODE.style.color = "black";
-        SWITCH_MODE.removeEventListener("mouseenter", mouserInHover);
-        SWITCH_MODE.removeEventListener("mouseleave", mouseOutHover);
+console.log(scroll_elements)
 
-        HEADER.style.background = "linear-gradient(145deg, #222 0%, #666 50%, #222 100%)";
-        HEADER.style.backgroundSize = "200% 200%";
-        HEADER.children[1].style.filter = "brightness(85%)";
+function navbar_action() {
+    if (navbar_status == 0) {
+        navbar_status = 1;
 
-        STUDIES.style.backgroundColor = "#0a0a0a";
-        STUDIES.style.color = "#eee";
+        navbar_trigger.textContent = "«";
+        navbar_trigger.style.left = "97.5%";
 
-        KNOWLEDGE.style.backgroundColor = "#0a0a0a";
-        KNOWLEDGE.children[0].style.color = "#eee";
-        KNOWLEDGE.children[0].style.borderColor = "#eee";
-        KNOWLEDGE.style.filter = "brightness(80%)";
-
-        FOOTER.style.backgroundColor = "#0a0a0a";
-        FOOTER.style.color = "#eee";
-
-        LINKEDIN_ICON.children[1].style.backgroundColor = "#eee";
-        LINKEDIN_ICON.children[1].style.color = "black";
-
-        LINKEDIN_QR.style.borderColor = "#444";
-        LINKEDIN_QR.style.filter = "invert(100%)";
-
-        // SVG loop and use too Flags images
-        for (let i = 0; i < SVG.length; i++) {
-            const element_svgs = SVG[i];
-            const element_cuadBox = LANGUAGES_CUAD_BOX[i];
-
-            element_svgs.style.fill = "#eee";
-            element_cuadBox.children[0].style.filter = "brightness(85%)";
-        };
-
-        // Icons loop
-        for (let t = 0; t < ICONS.length; t++) {
-            const element = ICONS[t];
-            element.style.filter = "invert(100%)";
-
-        };
-
-        if (screen.width >= 1000) {
-            mouserInHover = SWITCH_MODE.addEventListener("mouseenter", () => {
-                SWITCH_MODE.style.backgroundColor = "#d6d682";
-                SWITCH_MODE.style.borderColor = "#c6be23";
-            });
-
-            mouseOutHover = SWITCH_MODE.addEventListener("mouseleave", () => {
-                SWITCH_MODE.style.backgroundColor = "#e8e8c7";
-                SWITCH_MODE.style.borderColor = "#c4bf5c";
-            });
-        }
-        flag_mode = 1;
+        navbar.style.left = 0;
     } else {
-        SWITCH_MODE.children[0].removeAttribute("hidden");
-        SWITCH_MODE.children[1].setAttribute("hidden", "true");
-        SWITCH_MODE.style.backgroundColor = "#669";
-        SWITCH_MODE.style.borderColor = "#336";
-        SWITCH_MODE.style.color = "#eee";
-        SWITCH_MODE.removeEventListener("mouseenter", mouserInHover);
-        SWITCH_MODE.removeEventListener("mouseleave", mouseOutHover);
+        navbar_status = 0;
 
-        HEADER.style.background = "linear-gradient(145deg, #969696 0%, #bdbdbd 50%, #969696 100%)";
-        HEADER.style.backgroundSize = "200% 200%";
-        HEADER.children[1].style.filter = "";
+        navbar_trigger.textContent = "»";
+        navbar_trigger.style.left = "0%";
 
-        STUDIES.style.backgroundColor = "#eee";
-        STUDIES.style.color = "black";
-
-        KNOWLEDGE.style.backgroundColor = "#eee";
-        KNOWLEDGE.children[0].style.color = "black";
-        KNOWLEDGE.children[0].style.borderColor = "black";
-        KNOWLEDGE.style.filter = "";
-
-        FOOTER.style.backgroundColor = "#eee";
-        FOOTER.style.color = "black";
-
-        LINKEDIN_ICON.children[1].style.backgroundColor = "black";
-        LINKEDIN_ICON.children[1].style.color = "#eee";
-
-        LINKEDIN_QR.style.borderColor = "black";
-        LINKEDIN_QR.style.filter = "";
-
-        // SVG loop and use too Flags images
-        for (let i = 0; i < SVG.length; i++) {
-            const element = SVG[i];
-            const element_cuadBox = LANGUAGES_CUAD_BOX[i];
-            
-            element.style.fill = "black";
-            element_cuadBox.children[0].style.filter = "";
-
-        };
-
-        // Icons loop
-        for (let t = 0; t < ICONS.length; t++) {
-            const element = ICONS[t];
-            element.style.filter = "invert(0%)";
-        };
-
-        if (screen.width >= 1000) {
-            mouserInHover = SWITCH_MODE.addEventListener("mouseenter", () => {
-                SWITCH_MODE.style.backgroundColor = "#323c72";
-                SWITCH_MODE.style.borderColor = "#111";
-            });
-
-            mouseOutHover = SWITCH_MODE.addEventListener("mouseleave", () => {
-                SWITCH_MODE.style.backgroundColor = "#669";
-                SWITCH_MODE.style.borderColor = "#336";
-            });
-        }
-        flag_mode = 0;
+        navbar.style.left = "-430px";
     }
-});
-
-const TEXT_CONTAINERS = document.getElementsByClassName("text_container");
-
-// --- SVG config
-for (let i = 0; i < SVG.length; i++) {
-    const element_svg = SVG[i];
-    const element_container = TEXT_CONTAINERS[i];
-    
-    console.log(element_svg);
-    console.log(element_container);
-    
-    let px = 0;
-
-    px = element_container.children[0].clientHeight * element_container.children.length;
-
-    element_svg.setAttribute("height", px);
-    
 }
 
+navbar_trigger.addEventListener("click", navbar_action);
 
-// --- make knowlege scrolled with touch nav bar ---
-const SKILLL_NAV = document.getElementsByClassName("skill_nav")[0].children;
-const SCROLL_WIDTH = window.screen.width;
-const SLIDDER = document.getElementsByClassName("slidder")[0];
+for (let i = 0; i < navbar_links.length; i++) {
+    const n = navbar_links[i];
 
-function ClickToScroll(id) {
-    SLIDDER.scrollTo({
-        top: 0,
-        left: SCROLL_WIDTH * id,
-        behavior: "smooth",
-    });
+    console.log(n)
+
+    n.addEventListener("click", () => {
+        scroll_elements[i].scrollIntoView()
+        navbar_action()
+    })
+    
 }
-
-for (let i = 0; i < SKILLL_NAV.length; i++) {
-    const elem = SKILLL_NAV[i];
-
-    elem.addEventListener("click", () => {
-        ClickToScroll(i);
-    });
-}
-
-// --- Add information to cards ---
-const request = new XMLHttpRequest(); //get data from JSON
-request.open("GET", "rsc/skills.json");
-request.responseType = "json";
-request.send();
-
-request.onload = function () {
-    const DATOS = request.response;
-    const KEYS_DATOS = Object.keys(DATOS);
-
-    let slidder_sons = SLIDDER.children;
-    let classes = ["topics", "works"]
-
-    for (let i = 0; i < slidder_sons.length; i++) { //loop for sections
-        const CHILD = slidder_sons[i];
-
-        for (let p = 0; p < classes.length; p++) { //loop for clases [topics|works]
-            const BOX_CLASS = classes[p];
-
-            // get div "list-desing"
-            let box = CHILD.getElementsByClassName(BOX_CLASS)[0];
-
-            // make exception for second card
-            if (BOX_CLASS != "works") {
-                //create the title h3 element
-                let h3 = document.createElement("h3");
-                h3.innerText = KEYS_DATOS[i];
-                box.appendChild(h3);
-            }
-
-            if (p == 1 && DATOS[KEYS_DATOS[i]][p].length == 0) {
-                let element_li = document.createElement("li");
-                element_li.innerText = "Ninguno...  de momento..."
-                box.appendChild(element_li);
-
-            } else {
-                // add the sub data
-                for (let b = 0; b < DATOS[KEYS_DATOS[i]][p].length; b++) { //loop for create each <li>
-                    const LI = DATOS[KEYS_DATOS[i]][p][b]; // DATO [ array_with_keys[iterator_of_section] ] [iterator_topics/work] [iterator_to_get_data]
-
-                    let element_li = document.createElement("li");
-                    element_li.innerText = LI;
-                    box.appendChild(element_li);
-                }
-            }
-        }
-
-    }
-};
